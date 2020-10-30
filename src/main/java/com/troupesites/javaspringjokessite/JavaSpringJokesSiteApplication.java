4503bb67e6f5123ae789c178a83d661543c9485e
Chris Troupe <chris.troupe@credera.com>
2020-10-30 07:54:32 -0500

    First commit for the project

---
+++ src/main/java/com/troupesites/javaspringjokessite/JavaSpringJokesSiteApplication.java
@@ -0,0 +1,13 @@
+package com.troupesites.javaspringjokessite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJokesSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringJokesSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/troupesites/javaspringjokessite/JavaSpringJokesSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troupesites.javaspringjokessite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringJokesSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
